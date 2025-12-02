83bc46ab37565e733abc3c513b5d835bc75799e3
RotColton <maromidomingo@gmail.com>
2025-12-02 13:27:50 +0100

    fix: change download cv behavior. It now opens in a new tab

--- src/components/Console/commands/DownloadCV.js
+++ src/components/Console/commands/DownloadCV.js
@@ -1,25 +1,8 @@
-// DownloadCV.js
-
-import axios from 'axios';
-
-const downloadFile = async () => {
-  try {   
-    const response = await axios.get(`${process.env.REACT_APP_WEB_SERVICE}${process.env.REACT_APP_DOWNLOAD_RESUME}`, {
-      responseType: 'blob',
-    });
-
-    const url = window.URL.createObjectURL(new Blob([response.data]));
-    const link = document.createElement('a');
-    link.href = url;
-    link.setAttribute('download', 'resume.pdf');
-
-    document.body.appendChild(link);
-    link.click();
-
-    link.parentNode.removeChild(link);
-  } catch (error) {
-    console.error('Error al descargar el CV:', error);
-  }
+const downloadFile = () => {
+  window.open(
+    `${process.env.REACT_APP_WEB_SERVICE}${process.env.REACT_APP_DOWNLOAD_RESUME}`,
+    '_blank'
+  );
 };
 
 export default downloadFile;
